220d880976ba2ba9a7d8c5c42ee5852025958565
yulia elkina <todg@mail.ru>
2021-02-08 19:10:17 +0300

    logout & current added

--- public/homePage.js
+++ public/homePage.js
@@ -1,2 +1,12 @@
 "use strict";
 const logoutButton = new LogoutButton;
+logoutButton.action = () => {
+  ApiConnector.logout((response) => {if (response.success) {
+    location.reload();
+  };}
+ )
+};
+ApiConnector.current((response) => {
+  if (response.success) {
+  ProfileWidget.showProfile(response.data);
+};})
