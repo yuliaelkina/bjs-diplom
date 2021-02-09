940c8ece5a3d70b658123d73c4e89e3360d381eb
yulia elkina <todg@mail.ru>
2021-02-09 11:45:41 +0300

    stockRates added

--- public/homePage.js
+++ public/homePage.js
@@ -3,10 +3,24 @@ const logoutButton = new LogoutButton;
 logoutButton.action = () => {
   ApiConnector.logout((response) => {if (response.success) {
     location.reload();
-  };}
+    };
+  }
  )
 };
 ApiConnector.current((response) => {
   if (response.success) {
     ProfileWidget.showProfile(response.data);
-};})
+};});
+
+const board = new RatesBoard;
+
+function getRates() {
+  ApiConnector.getStocks((response) => {
+    if (response.success) {
+      board.clearTable();
+      board.fillTable(response.data);
+    }
+  })
+};
+let timerRates = setInterval(getRates, 1000);
+
