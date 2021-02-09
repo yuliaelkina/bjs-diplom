9a519b7e564fe1e75258d61664b13b3c6703499b
yulia elkina <todg@mail.ru>
2021-02-09 14:15:19 +0300

    final

--- public/homePage.js
+++ public/homePage.js
@@ -20,8 +20,8 @@ function getRates() {
     if (response.success) {
       board.clearTable();
       board.fillTable(response.data);
-    }
-  })
+    };
+  });
 };
 const timerRates = setInterval(getRates, 1000);
 
@@ -49,4 +49,54 @@ money.conversionMoneyCallback = (data) => {
     };
   });
 };
+
+money.sendMoneyCallback = (data) => {
+  ApiConnector.transferMoney(data, (response) => {
+    if(response.success) {
+      ProfileWidget.showProfile(response.data);
+      money.setMessage(response.success, "Перевод произведен успешно");
+    }
+    else {
+      money.setMessage(response.success, response.error);
+    };
+  });
+};
+
+const favorites = new FavoritesWidget;
+ApiConnector.getFavorites((response) => {
+  if (response.success) {
+    favorites.clearTable();
+    favorites.fillTable(response.data);
+    money.updateUsersList(response.data);
+};});
+
+favorites.addUserCallback = (data) => {
+  ApiConnector.addUserToFavorites(data, (response) => {
+    if (response.success) {
+      favorites.clearTable();
+      favorites.fillTable(response.data);
+      money.updateUsersList(response.data);
+      favorites.setMessage(response.success, "Пользователь успешно добавлен");
+    }
+    else {
+      favorites.setMessage(response.success, response.error);
+    };
+  }
+  );
+};
+
+favorites.removeUserCallback = (data) => {
+  ApiConnector.removeUserFromFavorites(data, (response) => {
+    if (response.success) {
+      favorites.clearTable();
+      favorites.fillTable(response.data);
+      money.updateUsersList(response.data);
+      favorites.setMessage(response.success, "Пользователь успешно удален");
+    }
+    else {
+      favorites.setMessage(response.success, response.error);
+    };
+  }
+  );
+}
 
